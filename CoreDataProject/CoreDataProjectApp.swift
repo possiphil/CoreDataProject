e41673e31c4f94fd141147ab7d6356d86c0c6620
Philipp Sanktjohanser <venture@mein.gmx>
2022-12-23 06:13:24 +0100

    Initial Commit

---
+++ CoreDataProject/CoreDataProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataProjectApp.swift
+//  CoreDataProject
+//
+//  Created by Philipp Sanktjohanser on 23.12.22.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
